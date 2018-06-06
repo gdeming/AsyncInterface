6ce27283a11b85dc57d37841247ecdb00ea258eb
Gary Deming <gdeming@peoplenetonline.com>
2018-06-06 16:47:31 -0500

    Initial commit.

---
+++ AsyncInterface/MyClass.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+
+namespace AsyncInterface
+{
+    interface IMyInterface
+    {
+        Task TurnScreenOn();
+    }
+
+    class MyClass : IMyInterface
+    {
+        public static async Task Main()
+        {
+            IMyInterface myInterface = new MyClass();
+            await myInterface.TurnScreenOn();
+        }
+
+        public async Task TurnScreenOn()
+        {
+            await Task.Run(() => MyMethod());
+        }
+
+        private void MyMethod()
+        {
+            // do something clever here
+        }
+    }
+}
